cbdc9314a8dcc8633efa008d527935d68f9368ae
Brynn Crowley <littlehill723@gmail.com>
2024-01-24 22:24:59 -0800

    create login front end

--- account/login.php
+++ account/login.php
@@ -1,6 +1,5 @@
 <?php
 require_once '../config.php';
-require ROOT_DIR . '/vendor/autoload.php';
 
 ob_start();
 include ROOT_DIR . '/header.php';
@@ -8,18 +7,20 @@ include ROOT_DIR . '/header.php';
 
 
 
-
-
+ob_flush();
 ?>
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Order: Log In</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="/account/login.css">
     <script type="text/javascript" src="/account/login.js"></script>   
 
 </head>
 <body>
     <div class="login-container">
+        <h1 class="login-header">Log In!</h1>
         <form action="/account/process-login.php" method="post" target="__blank" class="login-form">
             <label for="email" class="entry-label">Email: </label><br>
             <input type="email" id="email" name="email" class="entry-box" placeholder="Enter Email" required><br>
@@ -30,9 +31,15 @@ include ROOT_DIR . '/header.php';
                 <label for="stay-signed-in" class="stay-signed-in">
                 Stay signed in <input type="checkbox" name="stay-signed-in" id="stay-signed-in">
                 </label><br>
-                <button id="submit" class="submit-button">Submit</button>
             </span>
+            <button id="submit" class="submit-button">Submit</button>
         </form>
+        <div class="separator">
+            <div class="line"></div>
+            <div class="text">OR</div>
+            <div class="line"></div>
+        </div>
+        <a href="/account/signup.php" class="signup">Create an account!</a>
 
     </div>
 
